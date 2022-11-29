0dd6d957566b8863926c34b611b560423c78ec7a
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-29 10:17:50 -0300

    criada a classe Produto.

---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,72 @@
+package com.generation.lojagames.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotBlank(message = "o atributo nome deve ser preenchido")
+    @Size(min = 5, max = 20, message = "o atributo nome deve ter no minimo 5 caracteres e no máximo 20 caracteres")
+    private String nome;
+
+    @NotBlank(message = "o atributo plataforma deve ser preenchido")
+    @Size(min = 2, max = 20, message = "o atributo plataforma deve ter no minimo 2 caracteres e no máximo 20 caracteres")
+    private String plataforma;
+
+    @NotNull(message = "o atributo preço não pode ser nulo")
+    @PositiveOrZero(message = "o atributo preço deve ser positivo")
+    private Double preco;
+
+    @NotBlank(message = "o atributo desenvolvedora deve ser preenchido")
+    @Size(min = 2, max = 20, message = "o atributo desenvolvedora deve ter no minimo 2 caracteres e no máximo 20")
+    private String desenvolvedora;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDesenvolvedora() {
+        return desenvolvedora;
+    }
+
+    public void setDesenvolvedora(String desenvolvedora) {
+        this.desenvolvedora = desenvolvedora;
+    }
+}
