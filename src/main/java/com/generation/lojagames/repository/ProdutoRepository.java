9dad2830483049de3dd7aa3c2832d136a34c21f4
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-29 10:57:52 -0300

    criada a interface ProdutoRepository.

---
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.generation.lojagames.repository;
+
+import com.generation.lojagames.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
