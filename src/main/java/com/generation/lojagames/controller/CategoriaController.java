2b0704d29b8c9a28fb1cbce0b9081e99aa0e43a0
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-29 19:15:56 -0300

    implementado métodos find all e findby id.

--- src/main/java/com/generation/lojagames/controller/CategoriaController.java
+++ src/main/java/com/generation/lojagames/controller/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.generation.lojagames.controller;
 
 
+import com.generation.lojagames.model.Categoria;
+import com.generation.lojagames.model.Produto;
 import com.generation.lojagames.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
@@ -13,4 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaController {
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll(){
+        return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity <Categoria> getById(@PathVariable Long id){
+        return categoriaRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
