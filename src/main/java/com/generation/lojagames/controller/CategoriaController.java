d6e4361d16f8597d085c9c5ba5eb724d7f6880c4
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-29 20:14:12 -0300

    implementado método delete.

--- src/main/java/com/generation/lojagames/controller/CategoriaController.java
+++ src/main/java/com/generation/lojagames/controller/CategoriaController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -39,5 +41,13 @@ public class CategoriaController {
     public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
 
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        Optional<Categoria> categoria= categoriaRepository.findById(id);
+        if (categoria.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        categoriaRepository.deleteById(id);
     }
 }
