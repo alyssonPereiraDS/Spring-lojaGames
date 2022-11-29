7a0ee905d610e39e830dfe6e2678d47900bd061f
Alysson Pereira <jose.apereira@mercadolivre.com>
2022-11-29 19:07:32 -0300

    criado método find by nome.

--- src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
+++ src/main/java/com/generation/lojagames/repository/CategoriaRepository.java
@@ -1,7 +1,12 @@
 package com.generation.lojagames.repository;
 
 import com.generation.lojagames.model.Categoria;
+import com.generation.lojagames.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
